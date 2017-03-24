d0aa3c83bd408ed399a7aa8b2e9464f56dc61b68
Iammiind <Onanong.pureemahawong@gmail.com>
2017-03-24 15:46:26 +0700

    [not finish] until 2.2

---
+++ src/main/java/camt/cbsd/dao/AbstractStudentDao.java
@@ -0,0 +1,28 @@
+package camt.cbsd.dao;
+
+import camt.cbsd.entity.Student;
+
+import java.util.List;
+
+/**
+ * Created by CAMT on 3/24/2017.
+ */
+public abstract class AbstractStudentDao implements StudentDao {
+    List<Student> students;
+    String baseUrl;
+    String imageUrl;
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    String imageBaseUrl;
+
+    @Override
+    public Student findById(long id) {
+        return students.stream().filter(s -> s.getId() == id).findFirst().orElse(null);
+    }
+}
